124c3785b9b9a12200f1865e8b05ab051b1c8cdb
Daniel Boateng <dannboateng@gmail.com>
2022-12-01 22:43:11 +0100

    magin and fast alphaber

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,16 +4,16 @@ if __name__ == "__main__":
     """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     from sys import argv
-	count = len(argv)
+    count = len(argv)
 
     if count - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
     if argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
